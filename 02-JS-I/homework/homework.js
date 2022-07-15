af1d19f5ff4896c9496af50c2a0d7caef0912757
Adrian <adrianx_13@hotmail.com>
2022-07-15 10:33:44 -0500

    ok

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 4.2;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 4.2;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  console.log('hola');
+  console.log("hola");
   return str;
   
 }
@@ -69,7 +69,13 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x == y) {
+    console.log (x == y);
+    return true
+  }
+  else{
+    return false;
+  }
 }
 
 function tienenMismaLongitud(str1, str2) {
