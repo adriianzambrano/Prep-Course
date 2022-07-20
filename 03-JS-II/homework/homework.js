f5edf976be278f01a4339eac73fa9c42f29078b5
Adrian <adrianx_13@hotmail.com>
2022-07-20 02:11:43 -0500

    29/36

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -133,7 +133,10 @@ function fizzBuzz(numero) {
   else {
     return numero;
   }
- 
+  // if(numero % 15 === 0) return 'fizzbuzz';
+  //if(numero % 3 === 0) return 'fizz';
+  //if(numero % 5 === 0) return 'buzz';
+  //return numero;
 
 }
 
@@ -144,6 +147,23 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+  if (0 > num1 || 0 > num2 || 0 > num3) {
+    return "Hay negativos";
+
+  } else if (0 === num1 || 0 ===  num2 || 0 === num3) {
+    return "Error";
+    
+  } else if (num1 > 0 && num1 > num2 && num1 > num3 ) {
+    return "Número 1 es mayor y positivo";
+
+  } else if (num3 > num1 && num3 > num2) {
+    return  num3 + 1;
+
+  } else{
+    return false;
+  }
+
 }
 
 function esPrimo(numero) {
